4425a1e693a47db5ea54a24f90a2bfb6ad9ef87d
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-28 09:43:11 +0530

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { Header, Footer } from "./components/";
-import { Home } from "./pages/";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, Explore, Liked, VideoPlay, Library, Playlist, Login, Signup } from "./pages/";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/liked" element={<Liked />} />
+        <Route path="/videoplay/:videoId" element={<VideoPlay />} />
+        <Route path="/playlist" element={<Playlist />} />
+        <Route path="/library" element={<Library />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 };
 
