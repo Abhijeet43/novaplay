e08124a1695d9bfa5a989e92546bfebf6436284a
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-29 11:31:18 +0530

    feat: added data to explore page

--- src/pages/Explore/components/TopCategory/TopCategory.jsx
+++ src/pages/Explore/components/TopCategory/TopCategory.jsx
@@ -1,28 +1,21 @@
 import React from "react";
 import "./TopCategory.css";
-import { NavLink } from "react-router-dom";
+import { useVideo } from "../../../../context/";
 
 const TopCategory = () => {
+  const {
+    videoState: { categories },
+  } = useVideo();
   return (
     <div className="categories-container">
-      <NavLink to="/explore/:all" className="btn btn-secondary">
-        All
-      </NavLink>
-      <NavLink to="/explore/1" className="btn btn-secondary">
-        Life Skils
-      </NavLink>
-      <NavLink to="/explore/2" className="btn btn-secondary">
-        Learning
-      </NavLink>
-      <NavLink to="/explore/3" className="btn btn-secondary">
-        Time Management
-      </NavLink>
-      <NavLink to="/explore/4" className="btn btn-secondary">
-        Science
-      </NavLink>
-      <NavLink to="/explore/5" className="btn btn-secondary">
-        Psycology
-      </NavLink>
+      <button className="btn btn-secondary">All</button>
+      {categories.map(({ categoryName, _id }) => {
+        return (
+          <button key={_id} className="btn btn-secondary">
+            {categoryName}
+          </button>
+        );
+      })}
     </div>
   );
 };
