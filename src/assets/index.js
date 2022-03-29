6ea8793478e1bd546dfc6079f87854a225e50313
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-29 11:23:19 +0530

    chore: added images for videos

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,37 @@
 import imageOne from "./imageOne.jpg";
+import imageTwo from "./imageTwo.jpg";
+import imageThree from "./imageThree.jpg";
+import imageFour from "./imageFour.jpg";
+import imageFive from "./imageFive.jpg";
+import imageSix from "./imageSix.jpg";
+import imageSeven from "./imageSeven.jpg";
+import imageEight from "./imageEight.jpg";
+import imageNine from "./imageNine.jpg";
+import imageTen from "./imageTen.jpg";
+import imageEleven from "./imageEleven.jpg";
+import imageTwelve from "./imageTwelve.jpg";
 import learning from "./learning.jpg";
 import time from "./time-mgmt.jpg";
 import lifeSkills from "./life-skills.jpg";
+import psychology from "./psychology.jpg";
+import science from "./science.jpg";
 
-export { imageOne, learning, time, lifeSkills };
+export {
+  imageOne,
+  imageTwo,
+  imageThree,
+  imageFour,
+  imageFive,
+  imageSix,
+  imageSeven,
+  imageEight,
+  imageNine,
+  imageTen,
+  imageEleven,
+  imageTwelve,
+  learning,
+  time,
+  lifeSkills,
+  psychology,
+  science,
+};
