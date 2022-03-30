06f7fcb985237000d0f506b78f2931bf4fa10e34
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-30 16:06:44 +0530

    feat: added liked videos on likes page

--- src/pages/Liked/components/LatestVideo/components/FeaturedStats/FeaturedStats.jsx
+++ src/pages/Liked/components/LatestVideo/components/FeaturedStats/FeaturedStats.jsx
@@ -4,7 +4,9 @@ import "./FeaturedStats.css";
 const FeaturedStats = ({ totalLikes, updated }) => {
   return (
     <div className="featured-stats">
-      <p>{totalLikes} videos</p>
+      <p>
+        <strong>{totalLikes}</strong> videos
+      </p>
       <span className="video-stats-dot"></span>
       <p>Updated {updated} days ago</p>
     </div>
