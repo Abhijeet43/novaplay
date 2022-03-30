a80d6662376765cb60491bedc7aca413c8781fa5
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-30 16:24:46 +0530

    fix: video count

--- src/pages/Liked/components/LatestVideo/components/FeaturedStats/FeaturedStats.jsx
+++ src/pages/Liked/components/LatestVideo/components/FeaturedStats/FeaturedStats.jsx
@@ -5,7 +5,9 @@ const FeaturedStats = ({ totalLikes, updated }) => {
   return (
     <div className="featured-stats">
       <p>
-        <strong>{totalLikes}</strong> videos
+        <strong>
+          {totalLikes} {totalLikes > 1 ? "videos" : "video"}
+        </strong>
       </p>
       <span className="video-stats-dot"></span>
       <p>Updated {updated} days ago</p>
