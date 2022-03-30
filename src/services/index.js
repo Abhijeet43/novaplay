0ccbfb158e0764af521e43d795cc50049e4557b4
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-03-30 16:07:29 +0530

    update: updated like functions in index

--- src/services/index.js
+++ src/services/index.js
@@ -2,3 +2,6 @@ export { getVideosService } from "./videoServices/getVideos.service";
 export { getCategoriesService } from "./videoServices/getCategories.service";
 export { loginService } from "./authServices/login.services";
 export { signUpService } from "./authServices/signup.services";
+export { addToLikesService } from "./likeServices/addToLikes.service";
+export { removeFromLikesService } from "./likeServices/removeFromLikes.service";
+export { getLikesService } from "./likeServices/getLikes.service";
