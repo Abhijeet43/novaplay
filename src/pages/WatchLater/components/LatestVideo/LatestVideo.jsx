c2e0f2efebb421e83d94dfc7b799694fca4b5288
Abhijeet43 <abhijeetsinha43@gmail.com>
2022-04-02 09:28:38 +0530

    update: updated watchLater for displaying videos

---
+++ src/pages/WatchLater/components/LatestVideo/LatestVideo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./LatestVideo.css";
+import { FeaturedStats } from "./components/FeaturedStats/FeaturedStats";
+import { ChannelDescription } from "./components/ChannelDescription/ChannelDescription";
+
+const LatestVideo = ({ image, totalLikes, updated, channelName, logo }) => {
+  return (
+    <div className="latest-video-featured">
+      <img src={image} alt="video-img" />
+      <h3 className="featured-title">Liked Videos</h3>
+      <FeaturedStats totalLikes={totalLikes} updated={updated} />
+      <hr />
+      <ChannelDescription channelName={channelName} logo={logo} />
+    </div>
+  );
+};
+
+export { LatestVideo };
